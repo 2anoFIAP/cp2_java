cbaa37b9bd6a7485857ff922914e4a65cf554497
guilherme de araujo <guiam2909@gmail.com>
2026-04-21 01:50:07 -0300

    feat: implementacao de regras de negocio, construtores e calculos de periodo na classe Aluno

--- src/br/com/fiap/bean/Aluno.java
+++ src/br/com/fiap/bean/Aluno.java
@@ -1,11 +1,57 @@
 package br.com.fiap.bean;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Aluno {
     private int registroMatricula;
     private String nomeCompleto;
     private LocalDate dataDeNascimento;
 
+    public Aluno(){
+    }
 
+    public Aluno(int registroMatricula, String nomeCompleto, LocalDate dataDeNascimento){
+        setDataDeNascimento(dataDeNascimento);
+        setNomeCompleto(nomeCompleto);
+        setRegistroMatricula(registroMatricula);
+    }
+
+    public int getRegistroMatricula(){
+        return registroMatricula;
+    }
+
+    public void setRegistroMatricula(int registroMatricula){
+        if (registroMatricula >= 80000 && registroMatricula <= 599999){
+            this.registroMatricula = registroMatricula;
+        } else {
+            System.out.println("Registro Invalido!");
+        }
+    }
+
+    public String getNomeCompleto(){
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto){
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public LocalDate getDataDeNascimento(){
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento){
+        LocalDate dataAtual = LocalDate.now();
+        if (dataDeNascimento.getYear() >= 1960 && !dataDeNascimento.isAfter(dataAtual)) {
+            this.dataDeNascimento = dataDeNascimento;
+        } else {
+            System.out.println("Data Invalida! Deve ser de 1960 em diante e nao pode ser futura.");
+        }
+    }
+
+    public String calcularIdadeCompleta(LocalDate dataAtual) {
+        Period periodo = Period.between(this.dataDeNascimento, dataAtual);
+        return String.format("%d anos, %d meses e %d dias.", periodo.getYears(), periodo.getMonths(), periodo.getDays());
+    }
 }
