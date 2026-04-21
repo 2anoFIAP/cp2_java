57a168a8aa2aac9486749646e1bdfe85e28c5d94
guilherme de araujo <guiam2909@gmail.com>
2026-04-21 01:09:32 -0300

    criando estruturas de pastas e classes

---
+++ src/br/com/fiap/bean/Aluno.java
@@ -0,0 +1,11 @@
+package br.com.fiap.bean;
+
+import java.time.LocalDate;
+
+public class Aluno {
+    private int registroMatricula;
+    private String nomeCompleto;
+    private LocalDate dataDeNascimento;
+
+
+}
